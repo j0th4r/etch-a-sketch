861da096652fd0382fb6e02a800c844f10e078a9
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-14 17:59:08 +0800

    Implement a progressive darkening effect

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const container = document.getElementById('container');
 const btn = document.getElementById('btn');
 let gridSize;
-
+let fade = 11;
 
 btn.addEventListener('click', () => {
+  fade = 11;
+
   const divs = document.querySelectorAll('.grid');
   divs.forEach((div) => {
     container.removeChild(div);
   });
-  
+
   do {
     gridSize = prompt('Enter the grid size');
   } while (gridSize < 1 || gridSize > 100);
@@ -19,10 +21,15 @@ btn.addEventListener('click', () => {
     div.classList.add('grid');
     div.style.width = `${100 / gridSize}%`;
     div.style.height = `${100 / gridSize}%`;
+   
     container.appendChild(div);
 
     div.addEventListener('mouseover', () => {
       div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+      if(fade != 0) {
+        fade--;
+      }
+    div.style.opacity = (fade/10);
     });
   }
 })
@@ -34,6 +41,10 @@ for (let i = 0; i < 16 * 16; i++) {
 
   div.addEventListener('mouseover', () => {
     div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    if(fade != 0) {
+        fade--;
+      }
+    div.style.opacity = (fade/10);
   });
 }
 
