d60c694a71e950fe943d60c072373937485e7bd5
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-14 17:18:30 +0800

    Implement random rgb trailing effect & Add button to create new sketch

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const container = document.getElementById('container');
+const btn = document.getElementById('btn');
+let gridSize;
+
+
+btn.addEventListener('click', () => {
+  const divs = document.querySelectorAll('.grid');
+  divs.forEach((div) => {
+    container.removeChild(div);
+  });
+  
+  do {
+    gridSize = prompt('Enter the grid size');
+  } while (gridSize < 1 || gridSize > 100);
+  
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    div.style.width = `${100 / gridSize}%`;
+    div.style.height = `${100 / gridSize}%`;
+    container.appendChild(div);
+
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    });
+  }
+})
 
 for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement('div');
   div.classList.add('grid');
   container.appendChild(div);
+
+  div.addEventListener('mouseover', () => {
+    div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  });
 }
+
